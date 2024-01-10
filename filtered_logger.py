7722dee753ca858f45ab1eb16167bb682d9a2a48
Albert <albertirura11@gmail.com>
2024-01-10 19:00:59 +0300

    custom logger formatter

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 """Defines a filter function"""
 import re
-
-
-def filter_datum2(fields, redaction, message, seperator):
-    """Obfuscate fileds in message with redaction"""
-    message = message.split(seperator)
-    for field in fields:
-        for m in message:
-            if re.match(field, m):
-                message[message.index(m)] = str(re.sub('=.*', '=' + redaction, m))
-    return ';'.join(message)
+import logging
+from datetime import datetime
 
 
 def filter_datum(fields, redaction, message, seperator):
@@ -18,5 +10,22 @@ def filter_datum(fields, redaction, message, seperator):
     """
     message = message.replace(seperator, ' ')
     for field in fields:
-        message = str(re.sub(field + '=\w(\w|\/)*', field + '=' + redaction, message))
+        message = str(re.sub(field + '=\w(\w|\/|@|\.|\-)*', field + '=' + redaction, message))
     return message.replace(' ', seperator)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        RedactingFormatter.FIELDS = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        string = filter_datum(self.FIELDS, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return str(self.FORMAT % {'name': record.name, 'levelname': record.levelname, 'asctime': datetime.utcnow(), 'message': string})
