6af3fd01e129fd69fb058749fbf976048f20d697
Albert <albertirura11@gmail.com>
2024-01-13 18:06:51 +0300

    password handling with bcrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Bycrypt module"""
+import bcrypt
+
+
+def hash_password(pwd):
+    """Hash user passwords.
+        They should never be stored
+        in plain text
+    """
+    # Unicode-objects must be encoded before hashing
+    utf8_pwd = pwd.encode('UTF-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(utf8_pwd, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check password validity
+    """
+    password = password.encode('UTF-8')
+    return bcrypt.checkpw(password, hashed_password)
