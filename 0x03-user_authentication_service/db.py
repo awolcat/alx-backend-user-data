3b0061126785a4aee98bffe0e602f72869ef7127
Albert <albertirura11@gmail.com>
2024-01-23 16:52:55 +0300

    type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from typing import TypeVar
 from user import Base, User
 
 
@@ -30,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add and persist a new user
         """
         session = self._session
